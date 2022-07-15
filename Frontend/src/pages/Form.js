192b858dd6e32d0f2f8b10b04914502b152aec6c
kahstan <kahsheng@gmail.com>
2022-07-15 17:41:06 +0800

    Update Form.js

--- Frontend/src/pages/Form.js
+++ Frontend/src/pages/Form.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 const Form = () => {
-  return <h1>This is Form</h1>;
+  return (
+    <>
+      <form>
+        <div>
+          <input type="text" placeholder="Title of Listing"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Pet Name"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Species"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Breed"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Sex"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Size"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Age of Pet"></input>
+        </div>
+        <div>
+          <input type="text" placeholder="Medical Conditions"></input>
+        </div>
+        <div>
+          <button>Submit Pet!</button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Form;
