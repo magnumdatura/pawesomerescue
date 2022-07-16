b3b832a9bc0620324a40db4697c3976639561317
kahstan <kahsheng@gmail.com>
2022-07-16 16:51:09 +0800

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,30 @@
 require("dotenv").config();
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
 const cors = require("cors");
+
 const connectDB = require("./db/db");
 const users = require("./router/users");
 const listings = require("./router/listings");
-const bodyParser = require("body-parser");
 
+const upload = multer();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 connectDB(process.env.MONGODB_URI);
+
+// for parsing application/xwww-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static("public"));
 
 app.use("/users", users); // {{server}}/users/
 app.use("/listings", listings); // {{server}}/listings/
