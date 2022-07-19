85e6f65e5439bd65bcfe3212146d4452498ed67d
magnumdatura <105579941+magnumdatura@users.noreply.github.com>
2022-07-19 14:13:53 +0800

    cleaned up LoginDetails

--- Frontend/src/pages/LoginDetails.js
+++ Frontend/src/pages/LoginDetails.js
@@ -75,70 +75,6 @@ const LoginDetails = () => {
     fetchDisplay("http://localhost:5001/users/users"); // eslint-disable-next-line
   }, []);
 
-  ////////////////
-  // Display users favorites :: WIP :: LIONEL
-  ////////////////
-
-  const displayUserFavourites = async (listingId) => {
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: "Bearer " + reactCtx.access,
-      },
-    };
-
-    try {
-      const res = await fetch(
-        "http://localhost:5001/listings/displayAll",
-        options
-      );
-
-      if (res.status !== 200) {
-        throw new Error("Could not fetch favourites from listing database.");
-      }
-
-      const data = await res.json();
-      console.log(data);
-
-      function filterByValue(array, string) {
-        return array.filter((o) => {
-          // .filter will return array of the values that passed .some (boolean) test
-          return Object.keys(o).some((k) => {
-            if (typeof o[k] === "string") return o[k].includes(string);
-          });
-        });
-      }
-
-      const filterFavourites = filterByValue(data, listingId);
-      console.log(filterFavourites);
-      return (
-        <span>
-          {/* <div>
-            <img src={filterFavourites[0].image} />
-          </div> */}
-          <div>
-            <h3>Name: {filterFavourites[0].petName}</h3>
-          </div>
-        </span>
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  console.log(reactCtx.userProfile);
-
-  // const hardcodeId = reactCtx.displayAll[0].favourites[1];
-  // console.log(hardcodeId);
-  // displayUserFavourites(hardcodeId)
-
-  // console.log(favouritesArr)
-
-  ////////////////
-  // WIP :: LIONEL
-  //////////////////
-
   return (
     <div className="App">
       <div>
@@ -166,29 +102,6 @@ const LoginDetails = () => {
                   </div>
                   <div>
                     <p>Role: {data.role}</p>
-                  </div>
-                  <div>
-                    <h2>
-                      Favourites:
-                      {data.favourites.map((favouriteID, index) => {
-                        const displayFavourites =
-                          displayUserFavourites(favouriteID);
-                        console.log(displayFavourites);
-                        // return (
-                        //   <span>
-                        //     <div>
-                        //       <h4>{displayFavourites}</h4>
-                        //     </div>
-                        //   </span>
-                        // );
-
-                        // {return (
-                        //   <div key={index}>
-                        //     <p>{favouriteID}</p>
-                        //   </div>
-                        // )};
-                      })}
-                    </h2>
                   </div>
                   <div>
                     <button id={data._id} onClick={handleDelete}>
