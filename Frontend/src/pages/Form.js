a5773e999a9584373cbbfe218bcf6e49b7434183
kahstan <kahsheng@gmail.com>
2022-07-20 23:09:22 +0800

    tagline

--- Frontend/src/pages/Form.js
+++ Frontend/src/pages/Form.js
@@ -114,8 +114,11 @@ const Form = () => {
       ) : (
         <form onSubmit={handleSubmit} encType="multipart/form-data">
           <div>
-            <h3 className="text-center mx-auto m-2 w-1/3 block w-50 px-3 py-2">
+            <h3 className=" text-2xl text-center mx-auto m-2 w-1/3 block w-50 px-3 py-2">
               Submit An Adoption!
+            </h3>
+            <h3 className=" text-l text-center mx-auto m-2 w-1/3 block w-50 px-3 italic">
+              <strong>We..</strong> thank you, for not abandoning your furchild.
             </h3>
           </div>
           <div className="grid grid-cols-6 gap-2">
